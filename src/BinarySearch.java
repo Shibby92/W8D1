2c461bbd99d09d7a75971704de18834f828f7a4c
Shibby92 <haris_000@hotmail.com>
2015-01-06 12:12:52 +0100

    Komentari dodani

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,7 +1,15 @@
+/**
+ * Program ispisuje broj indeksa clana niza ako trazeni broj od korisnika
+ * postoji u nizu,a vraca -1 ako taj broj ne postoji u tom nizu
+ * 
+ * @author harisarifovic
+ *
+ */
 public class BinarySearch {
 
 	public static void main(String[] args) {
 		int[] niz = new int[] { 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };
+		// ispitivanje niza
 		for (int i = 20; i < 32; i++)
 			System.out.println(binarySearch(niz, i, 0, niz.length));
 		int unos = TextIO.getInt();
@@ -10,6 +18,13 @@ public class BinarySearch {
 
 	}
 
+	/**
+	 * Rekurzivno sabiranje brojeva od nekog broja do nule
+	 * 
+	 * @param unos
+	 *            pocetak sabiranja
+	 * @return sumu tih brojeva
+	 */
 	private static int rekSum(int unos) {
 		if (unos > 0)
 			unos += rekSum(unos - 1);
@@ -17,6 +32,12 @@ public class BinarySearch {
 
 	}
 
+	/**
+	 * Ispisuje brojeve od unosa do nule
+	 * 
+	 * @param unos
+	 *            Broj od kojeg pocinje ispis
+	 */
 	private static void rekPrint(int unos) {
 		if (unos < 0) {
 			return;
@@ -25,19 +46,33 @@ public class BinarySearch {
 		rekPrint(unos - 1);
 	}
 
+	/**
+	 * Rekurzivna funkcija koja trazi uneseni korisnicki broj
+	 * 
+	 * @param array
+	 *            Niz u kojem se trazi broj
+	 * @param find
+	 *            Broj koji se trazi
+	 * @param start
+	 *            pocetak niza
+	 * @param end
+	 *            kraj niza
+	 * @return indeks na kojem se nalazi niz, osim ako ga nije nasao, onda vraca
+	 *         -1
+	 */
 	public static int binarySearch(int[] array, int find, int start, int end) {
 		if (start >= end) {
-			return-1;
-			}
-			int startEnd = (start + end) / 2;
-			if (find == array[startEnd]) {
-				return startEnd;
-			} else if (find < array[startEnd]) {
-				return binarySearch(array, find, start, startEnd);
-			} else if (find > array[startEnd]) {
-				return binarySearch(array, find, startEnd +1, end);
-			}
-		
-		return -1;
+			return -1;
 		}
+		int startEnd = (start + end) / 2;
+		if (find == array[startEnd]) {
+			return startEnd;
+		} else if (find < array[startEnd]) {
+			return binarySearch(array, find, start, startEnd);
+		} else if (find > array[startEnd]) {
+			return binarySearch(array, find, startEnd + 1, end);
+		}
+
+		return -1;
+	}
 }
--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -7,7 +7,16 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+/**
+ * Kreira igru gdje korisnik zamisli broj izmedju 1 i 10000, racunar pokusava da
+ * pogodi taj broj, gdje korisnik govori da li je ponudjeni broj od racunara
+ * veci ili manji od zamisljenog
+ * 
+ * @author harisarifovic
+ *
+ */
 public class GuessTheNumber extends JPanel implements ActionListener {
+	// Kreira komponente potrebne za igru
 	private static JButton manje = new JButton("Manje");
 	private static JButton vise = new JButton("Vise");
 	private static JButton tacno = new JButton("Tacno");
@@ -18,19 +27,28 @@ public class GuessTheNumber extends JPanel implements ActionListener {
 	private static int end = 10000;
 	private static JFrame success;
 
+	/**
+	 * Postavlja listenere za buttone,kreira labele kao i prozor koji ce se
+	 * pokrenuti kada racunar pogodi zamisljeni broj
+	 */
 	public GuessTheNumber() {
 		manje.addActionListener(this);
 		vise.addActionListener(this);
 		tacno.addActionListener(this);
 		number = new JLabel(String.valueOf(Binary(start, end)));
 		counter = new JLabel();
-		success= new JFrame("WIN!");
+		success = new JFrame("WIN!");
 		success.setSize(200, 100);
 		success.setLocation(800, 600);
 		success.add(new JButton("SUCCESS!!!"));
-		
+
 	}
 
+	/**
+	 * Kreira prozor gdje ce se igrati igra
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 
 		JFrame window = new JFrame("GUESS THE NUMBER!!!");
@@ -46,11 +64,14 @@ public class GuessTheNumber extends JPanel implements ActionListener {
 		panel.add(vise, BorderLayout.EAST);
 		panel.add(counter, BorderLayout.SOUTH);
 		window.setVisible(true);
-		
 
 	}
 
 	@Override
+	/**
+	 * provjerava koji je button pritisnut, tako da program
+	 * reaguje ispravno
+	 */
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == manje) {
 			end = (start + end) / 2;
@@ -58,13 +79,24 @@ public class GuessTheNumber extends JPanel implements ActionListener {
 		} else if (e.getSource() == vise) {
 			start = (start + end) / 2;
 		}
+		// postavlja novi broj za pokusaj pogotka
 		number.setText(String.valueOf(Binary(start, end)));
+		// Pokazuje broj pokusaja
 		counter.setText(String.valueOf(++intCounter));
-		if(e.getSource()==tacno){
+		if (e.getSource() == tacno) {
 			success.setVisible(true);
 		}
 	}
 
+	/**
+	 * Racuna srednju vrijednost dva broja
+	 * 
+	 * @param start
+	 *            prvi broj
+	 * @param end
+	 *            drugi broj
+	 * @return srednju vrijednost dva broja
+	 */
 	private static int Binary(int start, int end) {
 		return (start + end) / 2;
 
